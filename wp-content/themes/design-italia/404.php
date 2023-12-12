f199bb36f118f6d793aef2b9fb63eb4363ce8d4f
Gaetano Caivano <gaetano.caivano@vertigocoop.it>
2023-12-12 19:31:32 +0100

    404 error fixeds

--- wp-content/themes/design-italia/404.php
+++ wp-content/themes/design-italia/404.php
@@ -1,7 +1,9 @@
 <?php 
 get_header();
 ?>
-Error 404
-<?php
+
+<p class="text-black titillium text-paragraph"><?php _e( 'Oops! La pagina che cerchi non è stata trovata,<br> <a href="javascript:history.back();" title="Torna alla pagina precedente">torna indietro</a> o utilizza il menu per continuare la navigazione.', 'design-italia' ); ?></p>
+                   
+       <?php
 get_footer( );
 ?>
