8c6f1abf4a66bad0dec810db942b8a144a5a7eac
Gaetano Caivano <gaetano.caivano@vertigocoop.it>
2024-01-02 16:16:52 +0100

    SVG url fixed

--- wp-content/themes/design-italia/searchform.php
+++ wp-content/themes/design-italia/searchform.php
@@ -5,13 +5,13 @@
                 <div class="modal-title">
                     <button class="search-link d-md-none" type="button" data-bs-toggle="modal" data-bs-target="#search-modal" aria-label="Chiudi e torna alla pagina precedente">
                         <svg class="icon icon-md">
-                            <use href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-arrow-left"></use>
+                            <use href="<?php echo get_template_directory_uri();?>/svg/sprites.svg#it-arrow-left"></use>
                         </svg>
                     </button>
                     <h2><?php _e('Cerca','design-italia');?></h2>
                     <button class="search-link d-none d-md-block" type="button" data-bs-toggle="modal" data-bs-target="#search-modal" aria-label="Chiudi e torna alla pagina precedente" data-focus-mouse="false">
                         <svg class="icon icon-md">
-                            <use href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-close-big"></use>
+                            <use href="<?php echo get_template_directory_uri();?>/svg/sprites.svg#it-close-big"></use>
                         </svg>
                     </button>
                 </div>
@@ -19,7 +19,7 @@
                     <label for="autocomplete-two" class="visually-hidden"><?php _e('Cerca nel sito','design-italia');?></label>
                     <input type="search" class="autocomplete ps-5" placeholder="<?php _e('Cerca nel sito','design-italia');?>" id="autocomplete-two" name="s" data-bs-autocomplete="[]" value="<?php echo esc_attr( get_search_query() ); ?>">
                     <span class="autocomplete-icon" aria-hidden="true">
-                        <svg class="icon"><use href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-search"></use></svg>
+                        <svg class="icon"><use href="<?php echo get_template_directory_uri();?>/svg/sprites.svg#it-search"></use></svg>
                     </span>
                     <button type="submit" class="btn btn-primary">
                         <span class=""><?php _e('Cerca','design-italia');?></span>
